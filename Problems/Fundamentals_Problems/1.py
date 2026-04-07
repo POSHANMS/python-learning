1513a700d7ba3bb623c0df5ce6b9f702c102e2e3
Poshan M S <siddeshwaraprasanna5@gmail.com>
2026-04-07 19:42:12 +0530

    Problems

---
+++ Problems/Fundamentals_Problems/1.py
@@ -0,0 +1,15 @@
+#Sum of N Numbers
+#Given an integer n find the sum of the first n natural number.
+def nSum(n):
+    ans = 0
+    ans=n*(n+1) // 2
+    return ans
+
+print(nSum(3))
+
+#In-Short
+def NSum(n):
+    return n * (n + 1) // 2
+
+print(NSum(5))
+print(NSum(10))
---
+++ Problems/Fundamentals_Problems/2.py
@@ -0,0 +1,7 @@
+#Int Str
+#Given three inputs that are stored in the variables a, b, and c. You need to print a a times and b b times  in a single line separated by c.
+
+a=int(input())
+b=int(input())
+c=(input())
+print(f"{str(a)*a}{c}{str(b)*b}")
